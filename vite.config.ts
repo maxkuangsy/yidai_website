c51a8911b965611e0cef550c2bc7a8aa9b1b3d81
Roy <64872041+maxkuangsy@users.noreply.github.com>
2024-11-10 23:37:02 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  base: '/yidai_website/'
+  base: '/yidai_website/',
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true, // 自动打开浏览器
+  },
+  build: {
+    outDir: 'dist', // 输出目录
+    sourcemap: true, // 构建后是否生成 source map 文件
+    chunkSizeWarningLimit: 1500, // 块大小警告的限制（以 kbs 为单位）
+  },
   resolve: {
     alias: {
-      '/public': '/public'
+      '@': path.resolve(__dirname, 'src')
     }
   },
   optimizeDeps: {
