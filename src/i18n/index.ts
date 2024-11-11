d53beb45a5850d2b5ade658c8377f03e52ea29d6
Roy <64872041+maxkuangsy@users.noreply.github.com>
2024-11-10 23:45:29 -0800

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslation from '/public/locales/en/translation.json';
-import zhTranslation from '/public/locales/zh/translation.json';
-import arTranslation from '/public/locales/ar/translation.json';
+import enTranslation from './locales/en/translation.json';
+import zhTranslation from './locales/zh/translation.json';
+import arTranslation from './locales/ar/translation.json';
 
 i18n
   .use(initReactI18next)
