b6ac5b0c16df7eb8e40beeffb84e29aac7340fd2
Roy <64872041+maxkuangsy@users.noreply.github.com>
2024-11-13 19:22:04 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/yidai_website/",
   plugins: [react()],
   optimizeDeps: {
     include: ["i18next", "react-i18next"],
